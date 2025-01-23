a967bdab57e5d77aee0511d3bbcd74342572d8a5
modest-hijab <ayah1328@aol.com>
2025-01-23 01:35:57 -0500

    js intro 8

--- js/assignment.js
+++ js/assignment.js
@@ -6,3 +6,41 @@ const largestNumberElement = document.getElementById("largest-number");
 const averageNumberElement = document.getElementById("average-number");
 
 const myNumbers = [35, 130, 354, 100, 25, 134, 99, 200, 35];
+
+function findSmallestNumber(array) {
+    let smallest = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] < smallest) {
+            smallest = array[i];
+        }
+    }
+    return smallest;
+}
+
+function findLargestNumber(array) {
+    let largest = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] > largest) {
+            largest = array[i];
+        }
+    }
+    return largest;
+}
+
+function findAverage(array) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    return sum / array.length;
+}
+
+function render() {
+    smallestNumberElement.textContent = findSmallestNumber(myNumbers); // Expected: 25
+    largestNumberElement.textContent = findLargestNumber(myNumbers); // Expected: 354
+    averageNumberElement.textContent = findAverage(myNumbers); // Expected: 123.55555555555556
+}
+
+submissionBtn.addEventListener("click", function () {
+    render();
+});
--- js/index.js
+++ js/index.js
@@ -1,3 +1,49 @@
 // Prevent us from attempting to use variables
 // that are not declared
-"use strict"
+
+
+"use strict";
+
+/*
+// Array of popcorn types
+const popcornTypes = ['Butter', 'Cheese', 'Caramel', 'Chocolate', 'Salted'];
+
+// Basic loop syntax
+for (let i = 0; i < popcornTypes.length; i++) {
+    console.log(popcornTypes[i]); // Output: Butter, Cheese, Caramel, Chocolate, Salted
+}
+
+// Using "for...of" syntax
+for (const type of popcornTypes) {
+    console.log(type); 
+    // Output: Butter, Cheese, Caramel, Chocolate, Salted
+}
+
+// Using Array.forEach() method
+popcornTypes.forEach(type => {
+    console.log(type); 
+    // Output: Butter, Cheese, Caramel, Chocolate, Salted
+});
+
+// Attempt to assign new values using "for...of"
+for (const type of popcornTypes) {
+    type = 'Gourmet'; 
+    // This won't change the original array
+}
+console.log(popcornTypes); 
+    // Output: Butter, Cheese, Caramel, Chocolate, Salted
+
+// Attempt to assign new values using Array.forEach()
+popcornTypes.forEach((type, index, array) => {
+    array[index] = 'Gourmet'; 
+    // This will change the original array
+});
+console.log(popcornTypes); 
+    // Output: Gourmet, Gourmet, Gourmet, Gourmet, Gourmet
+
+// Using "for...in" loop
+for (const index in popcornTypes) {
+    console.log(index + ': ' + popcornTypes[index]); 
+    // Output: 0: Gourmet, 1: Gourmet, 2: Gourmet, 3: Gourmet, 4: Gourmet
+}
+*/
